ad4d6f4d536eceb9474d7991b1dd4d9e126fcbbc
IrynaKolh <iryna.kolhanova@gmail.com>
2022-02-15 13:39:21 -0500

    fix brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2) return false;
+
+    const pairs = bracketsConfig.reduce((acc, [key, value]) => ({...acc, [key]: value }), {});
+    let result = [];
+    let string = str.split('');
+
+    for (let i = 0; i < string.length; i++) {
+        if (
+            string[i] in pairs &&
+            (string[i] != pairs[string[i]] || (string[i] != string[i + 1] && i != string.length - 1)) &&
+            pairs[result[result.length - 1]] != string[i]
+        ) {
+            result.push(string[i]);
+        } else if (pairs[result[result.length - 1]] == string[i]) {
+            result.pop();
+        } else if (string[i] == string[i + 1]) {
+            i++;
+            continue;
+        }
+    }
+
+    return !result.length;
 }
